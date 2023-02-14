f755170016c2e15e7c5cceb9ada037ae9a03c4f4
saku-bruh <notwolfyy@gmail.com>
2023-02-14 02:39:04 +0200

    extra-nagara: initial import

---
+++ extra.mk
@@ -0,0 +1,20 @@
+PRODUCT_SOONG_NAMESPACES += \
+    sony/extra-nagara
+
+# SonyFramework
+ PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/extra/sonyframework/system/,$(TARGET_COPY_OUT_SYSTEM)/) \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/extra/sonyframework/product/,$(TARGET_COPY_OUT_PRODUCT)/)
+
+# Camera
+ PRODUCT_PACKAGES += \
+    PhotoPro \
+    CinemaPro \
+    VideoPro \
+    CameraPanorama \
+    CreativeEffect \
+    PortraitSelfie
+
+# Controller Support
+ PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/extra/keylayout/,$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout)
